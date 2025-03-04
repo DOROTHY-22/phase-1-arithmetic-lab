68a81e22d09d363ea26f840e357946e428063866
DOROTHY CHEPKOECH <chepkoechdorothy209@gmail.com>
2025-03-04 21:43:03 +0300

    Finish assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 =14;
+let num4 =5;
+let mod = num3 % num4;
+let numbers = [5, 12, 20, 8, 15];
+let max = Math.max(...numbers); 
 
 // Write your code here
