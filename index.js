a33e33d17adcc7a3eeca47f50b64e98eb6558f08
DOROTHY CHEPKOECH <chepkoechdorothy209@gmail.com>
2025-03-08 19:10:47 +0300

    Make the few changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let num1 = 2;
 let num2 = 31;
-let multiply = num1 * num2;
-let random = Math.floor(Math.random() * 10) + 1;
+let multiply = num1 * num2;//numtiplies two numbers whose result is 62
+let random = Math.floor(Math.random() * 10) + 1;//random integer greater than 1
 let num3 =14;
 let num4 =5;
-let mod = num3 % num4;
+let mod = num3 % num4;//modulus 
 let numbers = [5, 12, 20, 8, 15];
-let max = Math.max(...numbers); 
+let max = Math.max(...numbers); // highest value in the set
 
 // Write your code here
